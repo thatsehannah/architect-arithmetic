ce8d29abd5c1902b00683edbe1889b178083ab07
E. Hannah <echannah631@gmail.com>
2023-08-10 19:50:37 -0500

    made changes to make calculations more dynamic

--- Program.cs
+++ Program.cs
@@ -9,56 +9,54 @@ namespace ArchitectArithmetic
             Console.Write("What monument would like to work with? ");
             string monument = Console.ReadLine();
             double totalCost = 0;
+            double totalArea = 0;
             switch (monument)
             {
                 case "Teotihuacan":
-                    totalCost = CalculateTotalCost(1500, 2500, 375, 750, 500);
+                    totalArea += Rect(2500, 1500);
+                    totalArea += Triangle(750, 500);
+                    totalArea += Circle(375) / 2;
+                    totalCost = CalculateTotalCost(totalArea);
                     break;
                 case "Taj Mahal":
-                    totalCost = CalculateTotalCost(rectLength: 90.5, rectHeight: 90.5, triBottom: 24, triHeight: 24);
+                    //calculate the area of the complete rectangle, then subtract the area of the four triangles
+                    totalArea += Rect(90.5 + 24, 90.5 + 24);
+                    totalArea -= 4 * Triangle(24, 24);
+                    totalCost = CalculateTotalCost(totalArea);
                     break;
                 case "Great Mosque":
-                    totalCost = CalculateTotalCost(rectLength: 264, rectHeight: 284, triBottom: 264, triHeight: 84);
-                    totalCost += CalculateTotalCost(rectLength: 180, rectHeight: 106);
+                    //calculate the area of the two complete rectangles, then subtract the area of the triangle
+                    totalArea += Rect(284, 264);
+                    totalArea += Rect(180, 106);
+                    totalArea -= Triangle(264, 84);
+                    totalCost = CalculateTotalCost(totalArea);
                     break;
             }
 
-            Console.WriteLine($"It will cost ${totalCost} to build {monument}.");
+            Console.WriteLine($"It will cost {totalCost} pesos to build the {monument}.");
         }
 
+        // in meters
         static double Rect(double length, double width)
         {
             return length * width;
         }
 
+        // in meters
         static double Circle(double radius)
         {
             return Math.PI * Math.Pow(radius, 2);
         }
 
+        // in meters
         static double Triangle(double bottom, double height)
         {
             return 0.5 * bottom * height;
         }
 
-        static double CalculateTotalCost(double rectLength = 0, double rectHeight = 0, double radius = 0, double triBottom = 0, double triHeight = 0)
+        // total cost in pesos
+        static double CalculateTotalCost(double totalArea)
         {
-            double totalArea = 0;
-            if (rectLength != 0 || rectHeight != 0)
-            {
-                totalArea += Rect(rectLength, rectHeight);
-            }
-
-            if (radius != 0)
-            {
-                totalArea += Circle(radius);
-            }
-
-            if (triBottom != 0 || triHeight != 0)
-            {
-                totalArea += Triangle(triBottom, triHeight);
-            }
-
             return Math.Round(180 * totalArea, 2);
         }
     }
